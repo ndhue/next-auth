03dcad52157886572920fa73216dfebe0b5c87fe
Ngo Diem Hue <diemhuengo@gmail.com>
2025-12-16 10:26:48 +0700

    create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,12 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import db from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+
+const saltRounds = 10;
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +15,22 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, name, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) return { error: "Email already in use!" };
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 };
